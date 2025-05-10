b10c373b4f48e478e3b23a7ebf0169115544043f
Yunyoung Jang <n0stress@naver.com>
2025-05-10 23:18:08 +0900

    commit

--- DeepDive_Algorithm/src/main/java/com/javatraining/datastructure/TheBackOfMyHeadFeelsHot.java
+++ DeepDive_Algorithm/src/main/java/com/javatraining/datastructure/TheBackOfMyHeadFeelsHot.java
@@ -1,47 +1,39 @@
-//package com.javatraining.datastructure;
-//
-//public class TheBackOfMyHeadFeelsHot {
-//
-//    import java.io.*;
-//import java.util.*;
-//
-//    public class Main {
-//
-//        public static void main(String[] args) throws IOException {
-//            // 입력 처리를 위한 BufferedReader
-//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//            StringBuilder sb = new StringBuilder(); // 출력 효율을 위한 StringBuilder
-//
-//            // N 입력 받기
-//            int N = Integer.parseInt(br.readLine());
-//
-//            // 봉우리 높이 배열 h
-//            int[] h = new int[N];
-//            String[] input = br.readLine().split(" ");
-//            for (int i = 0; i < N; i++) {
-//                h[i] = Integer.parseInt(input[i]);
-//            }
-//
-//            // 스택 선언
-//            Stack<Integer> stk = new Stack<>();
-//
-//            // 각 신선에 대해 봉우리 탐색 및 출력
-//            for (int i = 0; i < N; i++) {
-//                // 현재 스택에 남아 있는 신선들의 수를 출력
-//                sb.append(stk.size()).append(" ");
-//
-//                // 스택에 남아 있는 신선들의 봉우리 높이가 감소하는 형태가 되도록
-//                // 현재 신선의 봉우리 높이보다 낮거나 같은 모든 신선들을 pop
-//                while (!stk.isEmpty() && h[stk.peek()] <= h[i]) {
-//                    stk.pop();
-//                }
-//
-//                // 현재 신선을 스택에 추가
-//                stk.push(i);
-//            }
-//
-//            // 결과 출력
-//            System.out.println(sb.toString().trim());
-//        }
-//    }
-//}
+package com.javatraining.datastructure;
+
+import java.io.*;
+import java.util.*;
+
+public class TheBackOfMyHeadFeelsHot {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // Timeout 방지
+        StringBuilder sb = new StringBuilder();
+        // 봉우리의 수
+        int N = Integer.parseInt(br.readLine());
+
+        // 봉우리의 높이
+        String[] input = br.readLine().split(" ");
+        int[] h = new int[N];
+        for(int i = 0; i<N; i++){
+            h[i] = Integer.parseInt(input[i]);
+        }
+
+        // 스택 선언
+        Stack<Integer> stack = new Stack<>();
+        // 각 신선에 대해 봉우리 탐색 및 출력
+        for(int i = 0; i<N; i++){
+            // 스택에 남아있는 신선들의 수
+            sb.append(stack.size()).append(" ");
+
+            // 오른쪽에 있는 신선의 봉우리 높이가 현재 신선의 봉우리 높이보다 낮거나 같은 모든 신선들을 pop
+            while(!stack.isEmpty() && h[stack.peek()] <= h[i]){
+                stack.pop();
+            }
+
+            // 현재 신선 스택에 추가
+            stack.push(i);
+        }
+        System.out.println(sb.toString().trim());
+    }
+}
